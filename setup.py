70e6bb8cab7360144e1ffc3b57e5ede23b7f5fe5
noxan <noxan@redmoonstudios.de>
2012-08-01 21:25:17 +0200

    set version from package file in setup script

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 import os
 from setuptools import setup, find_packages
 
+import posts
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='django-posts',
+    version=posts.__version__,
     description='A generic posts app for django',
     long_description=read('README.md'),
 )
