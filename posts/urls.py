664619b5abfcb50c12e19414ad73e1dfc3bc3f24
noxan <noxan@redmoonstudios.de>
2012-08-01 22:11:37 +0200

    add posts detail view to urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
 
-from views import PostListView
+from views import PostListView, PostDetailView
 
 
 urlpatterns = patterns('',
     url(r'^$', PostListView.as_view(), name='list'),
+    url(r'^(?P<pk>\d+)/$', PostDetailView.as_view(), name='detail'),
 )
