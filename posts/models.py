6b9d6706840c479d50df89c75572f4221ae95a4a
noxan <noxan@redmoonstudios.de>
2012-08-01 21:58:56 +0200

    create very simple post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User)
+    headline = models.CharField(max_length=255)
+    text = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+
