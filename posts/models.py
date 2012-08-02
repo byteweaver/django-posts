53158fa2e89747c0d29de4543a4a42c944daf2ed
noxan <noxan@redmoonstudios.de>
2012-08-02 19:39:13 +0200

    flag author field in posts model as not editable

--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, editable=False)
     headline = models.CharField(max_length=255)
     text = models.TextField()
     creation_date = models.DateTimeField(auto_now_add=True)
