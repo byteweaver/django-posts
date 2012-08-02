3cceaa9c54e87f7b42b069a598e5186244a7a5fc
noxan <noxan@redmoonstudios.de>
2012-08-02 20:25:26 +0200

    remove unnecessary newlines from runtests script

--- posts/tests/runtests.py
+++ posts/tests/runtests.py
@@ -40,15 +40,12 @@ if not settings.configured:
     )
 
 
-
 class NoseCoverageTestRunner(CoverageRunner, NoseTestSuiteRunner):
     pass
-
 
 def runtests(*test_args):
     failures = NoseCoverageTestRunner(verbosity=2, interactive=True).run_tests(test_args)
     sys.exit(failures)
-
 
 if __name__ == '__main__':
     runtests(*sys.argv[1:])
