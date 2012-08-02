463cf24b343889d92a9a5c46cf593660704be99d
noxan <noxan@redmoonstudios.de>
2012-08-02 19:38:02 +0200

    add posts admin for using current user in request as author

--- posts/admin.py
+++ posts/admin.py
@@ -4,4 +4,12 @@ from django.contrib.auth.models import User
 from models import Post
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        try:
+            obj.author
+        except User.DoesNotExist:
+            obj.author = request.user
+        obj.save()
+
+admin.site.register(Post, PostAdmin)
