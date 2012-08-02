33c736de6c4f28bf7c8fb88fa3eba39190005ca8
noxan <noxan@redmoonstudios.de>
2012-08-02 19:15:30 +0200

    add count for limit to post templatetag

--- posts/templatetags/post_tags.py
+++ posts/templatetags/post_tags.py
@@ -6,10 +6,13 @@ from posts.models import Post
 register = template.Library()
 
 class PostListNode(template.Node):
+    def __init__(self, count):
+        self.count = count
+
     def render(self, context):
-        context['post_list'] = Post.objects.all()
+        context['post_list'] = Post.objects.all()[:self.count]
 
 @register.tag
 def get_post_list(parser, token):
-    return PostListNode()
+    return PostListNode(3)
 
