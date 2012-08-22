e53172f67cc6df83a65e5c142e83bb11c662894a
noxan <noxan@redmoonstudios.de>
2012-08-22 03:02:34 +0200

    rename post model into abstract post model and extend normal post from it

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Post(models.Model):
+class AbstractPost(models.Model):
     author = models.ForeignKey(User, editable=False)
     headline = models.CharField(max_length=255)
     text = models.TextField()
@@ -10,9 +10,14 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-creation_date']
+        abstract = True
 
     def __unicode__(self):
         return self.headline
+
+
+class Post(AbstractPost):
+    pass
 
 class PositionPostMixin(models.Model):
     position = models.IntegerField()
