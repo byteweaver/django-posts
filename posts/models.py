5171cbd29eae1d4d0a1bd760633620f0f347fa21
gilsondev <contato@gilsondev.com>
2013-03-06 16:26:14 -0300

    Insert translations in models

--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 
 class AbstractPost(models.Model):
     author = models.ForeignKey(User, editable=False)
-    headline = models.CharField(max_length=255)
-    text = models.TextField()
-    creation_date = models.DateTimeField(auto_now_add=True)
+    headline = models.CharField(_("Headline"), max_length=255)
+    text = models.TextField(_("Text"))
+    creation_date = models.DateTimeField(_("Creation Date"), auto_now_add=True)
 
     class Meta:
         ordering = ['-creation_date']
@@ -16,7 +17,7 @@ class AbstractPost(models.Model):
         return self.headline
 
 class Post(AbstractPost):
-    slug = models.SlugField(max_length=150)
+    slug = models.SlugField(_("Slug"), max_length=150)
 
     @models.permalink
     def get_absolute_url(self):
@@ -27,27 +28,27 @@ class VisibilityPostManagerMixin(models.Manager):
         return self.filter(visible=True)
 
 class VisibilityPostMixin(models.Model):
-    visible = models.BooleanField(default=True)
+    visible = models.BooleanField(_("Visible"), default=True)
 
     class Meta:
         abstract = True
 
 class PositionPostMixin(models.Model):
-    position = models.IntegerField()
+    position = models.IntegerField(_("Position"))
 
     class Meta:
         ordering = ['position']
         abstract = True
 
 class LinkPostMixin(models.Model):
-    link_url = models.URLField(blank=True)
-    link_name = models.CharField(max_length=255, blank=True)
+    link_url = models.URLField(_("Link URL"), blank=True)
+    link_name = models.CharField(_("Link Name"), max_length=255, blank=True)
 
     class Meta:
         abstract = True
 
 class ImagePostMixin(models.Model):
-    image = models.CharField(max_length=40)
+    image = models.CharField(_("Image"), max_length=40)
 
     class Meta:
         abstract = True
