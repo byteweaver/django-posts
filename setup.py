5db9518e9196ac927c4e7d08c22c2a972bbbc851
gilsondev <contato@gilsondev.com>
2013-03-06 14:24:40 -0300

    Inserting south for dependency

--- setup.py
+++ setup.py
@@ -10,16 +10,17 @@ def read(fname):
 setup(
     name='django-posts',
     version=posts.__version__,
-    description='A generic posts app for django',
+    description='A generic posts app for django (fork of the django-posts)',
     long_description=read('README.md'),
     license='MIT License',
-    author='noxan',
-    author_email='contact@byteweaver.net',
-    url='https://github.com/byteweaver/django-posts',
+    author='Gilson Filho',
+    author_email='contato@gilsondev.com',
+    url='https://github.com/gilsondev/django-posts',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'Django',
+        'South',
     ],
     tests_require=[
         'Django',
