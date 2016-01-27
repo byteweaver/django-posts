460e6608cb68995c6e9ce1d80e6797758c0c3803
Richard <hello@noxan.net>
2016-01-27 13:23:18 +0100

    upgrade url patterns to list syntax

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from posts.views import PostListView, PostDetailView
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', PostListView.as_view(), name='list'),
     url(r'^(?P<pk>[\d]+)/$', PostDetailView.as_view(), name='detail'),
     url(r'^(?P<slug>[-\w]+)/$', PostDetailView.as_view(), name='detail-by-slug'),
-)
+]
