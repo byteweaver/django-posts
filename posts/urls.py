ffaa75ce4b7587aefecb8dde0213f44ddf9346de
Richard <hello@noxan.net>
2016-01-27 13:23:26 +0100

    pep8 fix line length in urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -6,5 +6,6 @@ from posts.views import PostListView, PostDetailView
 urlpatterns = [
     url(r'^$', PostListView.as_view(), name='list'),
     url(r'^(?P<pk>[\d]+)/$', PostDetailView.as_view(), name='detail'),
-    url(r'^(?P<slug>[-\w]+)/$', PostDetailView.as_view(), name='detail-by-slug'),
+    url(r'^(?P<slug>[-\w]+)/$',
+        PostDetailView.as_view(), name='detail-by-slug'),
 ]
