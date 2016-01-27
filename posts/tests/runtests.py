e40ef027674943d9be5dd59a4ddbb2665c3e1634
Richard <hello@noxan.net>
2016-01-27 12:26:39 +0100

    pep8 fix runtests.py

--- posts/tests/runtests.py
+++ posts/tests/runtests.py
@@ -2,9 +2,11 @@
 
 import os
 import sys
-import multiprocessing
 
 from django.conf import settings
+
+from django_coverage.coverage_runner import CoverageRunner
+from django_nose import NoseTestSuiteRunner
 
 
 EXTERNAL_APPS = [
@@ -25,32 +27,35 @@ INTERNAL_APPS = [
 
 if not settings.configured:
     settings.configure(
-        DATABASES = {
+        DATABASES={
             'default': {
                 'ENGINE': 'django.db.backends.sqlite3',
                 'NAME': ':memory:',
             }
         },
-        INSTALLED_APPS = EXTERNAL_APPS + INTERNAL_APPS,
+        INSTALLED_APPS=EXTERNAL_APPS + INTERNAL_APPS,
         ROOT_URLCONF='posts.tests.urls',
-        TEMPLATE_DIRS = (
+        TEMPLATE_DIRS=(
             os.path.join(os.path.dirname(__file__), '../templates'),
         ),
-        COVERAGE_MODULE_EXCLUDES = EXTERNAL_APPS,
-        COVERAGE_REPORT_HTML_OUTPUT_DIR=os.path.join(os.path.dirname(__file__), 'coverage')
+        COVERAGE_MODULE_EXCLUDES=EXTERNAL_APPS,
+        COVERAGE_REPORT_HTML_OUTPUT_DIR=os.path.join(
+            os.path.dirname(__file__),
+            'coverage'
+        )
     )
 
-
-from django_coverage.coverage_runner import CoverageRunner
-from django_nose import NoseTestSuiteRunner
 
 class NoseCoverageTestRunner(CoverageRunner, NoseTestSuiteRunner):
     pass
 
+
 def runtests(*test_args):
-    failures = NoseCoverageTestRunner(verbosity=2, interactive=True).run_tests(test_args)
+    failures = NoseCoverageTestRunner(
+        verbosity=2,
+        interactive=True
+    ).run_tests(test_args)
     sys.exit(failures)
 
 if __name__ == '__main__':
     runtests(*sys.argv[1:])
-
